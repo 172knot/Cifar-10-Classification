c8531de6464c00a4984b2a39a9c9e975eb892b4e
Abhishek Pal <knot15a@gmail.com>
2018-03-21 01:03:44 +0530

    Base code for part 3

--- code3.py
+++ code3.py
@@ -5,32 +5,33 @@ import os
 import cv2
 
 path = '/home/knot/Documents/Semester6/Dl/Assignment1/cifar-10-python/cifar-10-batches-py/train_data/data_batch_1'
-batch_size = 8
-epochs = 3
-learning_rate = 0.01
+batch_size = 256
+epochs = 300
+learning_rate = 0.0001
 X = tf.placeholder(tf.float32, [None, 32, 32, 3])
 Y_ = tf.placeholder(tf.float32, [None, 10])
-lr = tf.Variable(0.01)
-drop = tf.placeholder(tf.float32)
+lr = tf.Variable(0.1)
+drop_1 = tf.placeholder(tf.float32)
+drop_2 = tf.placeholder(tf.float32)
 
 def G2_Net():
 
-    conv1 = tf.layers.conv2d(inputs = X, filters = 32, kernel_size = 3, padding = "same", activation = tf.nn.relu, name = "conv1")
+    conv1 = tf.layers.conv2d(inputs = X, filters = 32, kernel_size = 3, strides = 1, padding = "same", activation = tf.nn.relu, name = "conv1")
     b_norm1 = tf.layers.batch_normalization(inputs = conv1, axis = -1, momentum = 0.99, epsilon = 0.001, center = True, scale = True, name = "batch_norm1")
-    pool1 = tf.layers.max_pooling2d(inputs = b_norm1, pool_size = 2, strides=2, name = "max_pool1")
+    conv2 = tf.layers.conv2d(inputs = b_norm1, filters = 32, kernel_size = 3, strides = 1, padding = "same", activation = tf.nn.relu, name = "conv2")
+    pool1 = tf.layers.max_pooling2d(inputs = conv2, pool_size = 2, strides=2, name = "max_pool1")
+    drop1 = tf.layers.dropout(inputs = pool1,  rate = drop_1, noise_shape = None, seed = None, name = "dropout1")
 
-    conv2 = tf.layers.conv2d(inputs = pool1, filters = 64, kernel_size = 3, padding = "same", activation = tf.nn.relu, name = "conv2")
-    b_norm2 = tf.layers.batch_normalization(inputs = conv2, axis = -1, momentum = 0.99, epsilon = 0.001, center = True, scale = True, name = "batch_norm2")
+    conv3 = tf.layers.conv2d(inputs = drop1, filters = 64, kernel_size = 3, strides = 1, padding = "same", activation = tf.nn.relu, name = "conv3")
+    b_norm2 = tf.layers.batch_normalization(inputs = conv3, axis = -1, momentum = 0.99, epsilon = 0.001, center = True, scale = True, name = "batch_norm2")
+    conv4 = tf.layers.conv2d(inputs = b_norm2, filters = 64, kernel_size = 3, strides = 1, padding = "same", activation = tf.nn.relu, name = "conv4")
+    pool2 = tf.layers.max_pooling2d(inputs = conv4, pool_size = 2, strides=2, name = "max_pool2")
+    drop2 = tf.layers.dropout(inputs = pool2,  rate = drop_1, noise_shape = None, seed = None, name = "dropout2")
 
-    conv3 = tf.layers.conv2d(inputs = b_norm2, filters = 128, kernel_size = 3, padding = "same", activation = tf.nn.relu, name = "conv3")
-    b_norm3 = tf.layers.batch_normalization(inputs = conv3, axis = -1, momentum = 0.99, epsilon = 0.001, center = True, scale = True, name = "batch_norm3")
-    pool3 = tf.layers.max_pooling2d(inputs = b_norm3, pool_size = 2, strides=2, name = "max_pool3")
-
-    pool1_flat = tf.reshape(pool3, [-1, 8 * 8 * 128])
-    dense1 = tf.layers.dense(inputs = pool1_flat, units = 512, activation = tf.nn.relu, name = "dense1")
-    drop1 = tf.layers.dropout(inputs = dense1,  rate = drop, noise_shape = None, seed = None, name = "dropout")
-    dense2 = tf.layers.dense(inputs = drop1, units = 1024, activation = tf.nn.relu, name = "dense2")
-    logits = tf.layers.dense(inputs = dense2, units = 10)
+    pool1_flat = tf.reshape(drop2, [-1, 8 * 8 * 64])
+    dense1 = tf.layers.dense(inputs = pool1_flat, units = 512, activation = tf.nn.sigmoid, name = "dense1")
+    drop3 = tf.layers.dropout(inputs = dense1,  rate = drop_2, noise_shape = None, seed = None, name = "dropout3")
+    logits = tf.layers.dense(inputs = drop3, units = 10)
     out = tf.nn.softmax(logits, name="softmax_output")
 
     return out
@@ -51,12 +52,33 @@ def nn():
         temp[label[i]] = 1
         train_y.append(temp)
 
+
+
+
     train_x = []
-    for i in range(len(data)):
-        img  = data[i].reshape((32,32,3))
+    for i in range(int(len(data))):
+        img = np.zeros([32,32,3])
+        for j in range(len(data[i])):
+            if(j<1024):
+                temp = int(j/32)
+                temp2 = j - (32*temp)
+                img[temp][temp2][0] = data[i][j]
+            elif(j<2048):
+                temp = int((j-1024)/32)
+                temp2 = (j-1024) - (32*temp)
+                img[temp][temp2][1] = data[i][j]
+            else:
+                temp = int((j-2048)/32)
+                temp2 = (j-2048) - (32*temp)
+                img[temp][temp2][2] = data[i][j]
+        temp_ = i - (5*int(i/5))
+        cv2.imwrite("img{}.png".format(temp_),img)
         train_x.append(img)
-    epoch = 0
-    epoch_loss = 0
+
+    print(train_y[1])
+    print(train_y[16])
+    print(train_y[31])
+
     with tf.Session() as sess:
         sess.run(init)
         for epoch in range(epochs):
@@ -67,15 +89,23 @@ def nn():
                 end = i+batch_size
                 batch_x = np.array(train_x[start:end])
                 batch_y = np.array(train_y[start:end])
+                # print(batch_y[0]," 01")
+                # print(batch_y[1]," 02")
+                # print(batch_y[2]," 03")
+                # print(batch_y[3]," 04")
                 c = 0
-                _, c, pred = sess.run([optimizer, cost, prediction], feed_dict={X: batch_x, Y_: batch_y, lr: learning_rate, drop: 0.5})
+                _, c, pred = sess.run([optimizer, cost, prediction], feed_dict={X: batch_x, Y_: batch_y, lr: learning_rate, drop_1: 0.25, drop_2: 0.5})
+                # print(pred[0]," 1")
+                # print(pred[1]," 2")
+                # print(pred[2]," 3")
+                # print(pred[3]," 4")
                 epoch_loss += c
                 i+=batch_size
             print('Epoch', epoch+1, 'completed out of',epochs,'loss:',epoch_loss)
 
-        # correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+        # correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y_, 1))
         # accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        # print('Accuracy:',accuracy.eval({x: batch_x, y:mnist.test.labels}))
+        #print('Accuracy:',accuracy.eval({x:, y:}))
 
 def main():
     nn()
