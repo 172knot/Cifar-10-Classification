01f2500cac664fc7592c04b0caba5fa3eeff05dd
Abhishek Pal <knot15a@gmail.com>
2018-03-24 19:19:37 +0530

    Completed

--- code2.py
+++ code2.py
@@ -2,76 +2,121 @@ import tensorflow as tf
 import numpy as np
 import pickle
 import os
-import cv2
 
-path = '/home/knot/Documents/Semester6/Dl/Assignment1/cifar-10-python/cifar-10-batches-py/train_data/data_batch_1'
-batch_size = 8
-epochs = 3
-learning_rate = 0.01
+
+tf.reset_default_graph()
+
+logs_path = "./logs2"
+path = './data'
+
+batch_size = 100
+epochs = 50
+learning_rate = 0.001
 X = tf.placeholder(tf.float32, [None, 32, 32, 3])
 Y_ = tf.placeholder(tf.float32, [None, 10])
-lr = tf.Variable(0.01)
+lr = tf.Variable(0.1)
+drop_1 = tf.placeholder(tf.float32)
+drop_2 = tf.placeholder(tf.float32)
+best_acc = 0
 
-def convolution_neural_network():
+def next_batch(i):
+    fo = open(os.path.join(path,"data_batch_{}".format(i)), 'rb')
+    dict_ = pickle.load(fo, encoding='latin1')
+    label = dict_['labels']
+    data = dict_['data']
+    img = np.reshape(data,(10000,3,32,32))
+    img = np.transpose(img,(0,2,3,1))
 
-    conv1 = tf.layers.conv2d(inputs = X, filters = 32, kernel_size = 7, padding = "same", activation = tf.nn.relu)
-    b_norm1 = tf.layers.batch_normalization(inputs = conv1, axis = -1, momentum = 0.99, epsilon = 0.001, center = True, scale = True)
-    pool1 = tf.layers.max_pooling2d(inputs = b_norm1, pool_size = 2, strides=2)
+    label = np.reshape(label,(10000))
+    label = label.T
+    y_one_hot = np.zeros((label.size,10))
+    y_one_hot[np.arange(label.size),label] = 1
+    return img,y_one_hot
 
-    pool1_flat = tf.reshape(pool1, [-1, 16 * 16 * 32])
-    dense = tf.layers.dense(inputs = pool1_flat, units = 1024, activation = tf.nn.relu)
-    # dropout = tf.layers.dropout(inputs = dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
-    logits = tf.layers.dense(inputs = dense, units = 10)
+def test_batch():
+    fo = open(os.path.join(path,"test_batch"), 'rb')
+    dict_ = pickle.load(fo, encoding='latin1')
+    label = dict_['labels']
+    data = dict_['data']
+    img = np.reshape(data,(10000,3,32,32))
+    img = np.transpose(img,(0,2,3,1))
+
+    label = np.reshape(label,(10000))
+    label = label.T
+    y_one_hot = np.zeros((label.size,10))
+    y_one_hot[np.arange(label.size),label] = 1
+    return img,y_one_hot
+
+def G2_Net():
+
+    conv1 = tf.layers.conv2d(inputs = X, filters = 32, kernel_size = 7, strides = 1, padding = "same", activation = tf.nn.relu, name = "conv1")
+    b_norm1 = tf.layers.batch_normalization(inputs = conv1, axis = -1, momentum = 0.99, epsilon = 0.001, center = True, scale = True, name = "batch_norm1")
+    pool1 = tf.layers.max_pooling2d(inputs = b_norm1, pool_size = 2, strides=2, name = "max_pool1")
+
+    pool1_flat = tf.reshape(pool1, [-1, 16 * 16* 32])
+    dense1 = tf.layers.dense(inputs = pool1_flat, units = 1024, activation = tf.nn.relu, name = "dense1")
+    logits = tf.layers.dense(inputs = dense1, units = 10)
     out = tf.nn.softmax(logits, name="softmax_output")
 
     return out
 
 def nn():
-    prediction = convolution_neural_network()
+    prediction = G2_Net()
     cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits = prediction,labels = Y_))
     optimizer = tf.train.AdamOptimizer(lr).minimize(cost)
+    correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+
+    tf.summary.scalar("cost", cost)
+    tf.summary.scalar("accuracy", accuracy)
+
+    summary_op = tf.summary.merge_all()
     init = tf.global_variables_initializer()
-    fo = open(path, 'rb')
-    dict_ = pickle.load(fo, encoding='latin1')
-    label = dict_['labels']
-    data = dict_['data']
+    saver = tf.train.Saver()
+    writer = tf.summary.FileWriter(logs_path, graph=tf.get_default_graph())
 
-    train_y = []
-    for i in range(len(label)):
-        temp = np.zeros(10)
-        temp[label[i]] = 1
-        train_y.append(temp)
-
-    train_x = []
-    for i in range(len(data)):
-        img  = data[i].reshape((32,32,3))
-        train_x.append(img)
-    epoch = 0
-    epoch_loss = 0
     with tf.Session() as sess:
-        sess.run(init)
+        saver.restore(sess,"./freeze1/model.ckpt")
+        #sess.run(init)
+        pt = 0
         for epoch in range(epochs):
             epoch_loss = 0
-            i = 0
-            while i < (2):
-                start = i
-                end = i+batch_size
-                batch_x = np.array(train_x[start:end])
-                batch_y = np.array(train_y[start:end])
-                _, c, pred = sess.run([optimizer, cost, prediction], feed_dict={X: batch_x, Y_: batch_y, lr: learning_rate})
-                print(pred.shape)
-                epoch_loss += c
-                i+=batch_size
-            print('Epoch', epoch+1, 'completed out of',epochs,'loss:',epoch_loss)
-
-        # correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
-        # accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        # print('Accuracy:',accuracy.eval({x: batch_x, y:mnist.test.labels}))
+            for j in range(4):
+                train_x, train_y = next_batch(j+1)
+                train_size = len(train_x)
+                i =0
+                while i < (train_size-batch_size):
+                    start = i
+                    end = i+batch_size
+                    batch_x = np.array(train_x[start:end])
+                    batch_x = batch_x/255
+                    batch_y = np.array(train_y[start:end])
+                    c = 0
+                    _, c, pred, summary, acc = sess.run([optimizer, cost, prediction, summary_op, accuracy], feed_dict={X: batch_x, Y_: batch_y, lr: learning_rate, drop_1: 0.25, drop_2: 0.5})
+                    writer.add_summary(summary, pt)
+                    pt += 1
+                    epoch_loss += c
+                    i+=batch_size
+            save_path = saver.save(sess, "./freeze1/model.ckpt")
+            batch_x, batch_y = next_batch(5)
+            batch_x = batch_x/255
+            sum_ = 0
+            for i__ in range(0,10000,100):
+                val_acc = accuracy.eval({X: batch_x[i__:i__+100], Y_:batch_y[i__:i__+100]})
+                sum_ += val_acc
+            sum_ = sum_/100
+            print(' Epoch: ',epoch+1,' completed out of: ',epochs,' train_acc: ', acc, ' val_acc: ', sum_)
+        batch_x, batch_y = test_batch()
+        batch_x = batch_x/255
+        sum_ = 0
+        for i__ in range(0,10000,100):
+            val_acc = accuracy.eval({X: batch_x[i__:i__+100], Y_:batch_y[i__:i__+100]})
+            sum_ += val_acc
+        sum_ = sum_/100
+        print(' Test_acc: ', sum_)
 
 def main():
     nn()
-
-
 
 if __name__ == "__main__":
     main()
--- code3.py
+++ code3.py
@@ -2,49 +2,71 @@ import tensorflow as tf
 import numpy as np
 import pickle
 import os
-import cv2
 
 
 tf.reset_default_graph()
 
-logs_path = "./logs2"
-path = '/home/knot/Documents/Semester6/Dl/Assignment1_/cifar-10-python/cifar-10-batches-py/train_data/data_batch_1'
+logs_path = "./logs"
+path = './data'
 
-batch_size = 256
-epochs = 150
-learning_rate = 0.001
+batch_size = 100
+epochs = 50
+learning_rate = 0.0001
 X = tf.placeholder(tf.float32, [None, 32, 32, 3])
 Y_ = tf.placeholder(tf.float32, [None, 10])
 lr = tf.Variable(0.1)
 drop_1 = tf.placeholder(tf.float32)
 drop_2 = tf.placeholder(tf.float32)
+best_acc = 0
+
+def next_batch(i):
+    fo = open(os.path.join(path,"data_batch_{}".format(i)), 'rb')
+    dict_ = pickle.load(fo, encoding='latin1')
+    label = dict_['labels']
+    data = dict_['data']
+    img = np.reshape(data,(10000,3,32,32))
+    img = np.transpose(img,(0,2,3,1))
+
+    label = np.reshape(label,(10000))
+    label = label.T
+    y_one_hot = np.zeros((label.size,10))
+    y_one_hot[np.arange(label.size),label] = 1
+    return img,y_one_hot
+
+def test_batch():
+    fo = open(os.path.join(path,"test_batch"), 'rb')
+    dict_ = pickle.load(fo, encoding='latin1')
+    label = dict_['labels']
+    data = dict_['data']
+    img = np.reshape(data,(10000,3,32,32))
+    img = np.transpose(img,(0,2,3,1))
+
+    label = np.reshape(label,(10000))
+    label = label.T
+    y_one_hot = np.zeros((label.size,10))
+    y_one_hot[np.arange(label.size),label] = 1
+    return img,y_one_hot
 
 def G2_Net():
 
-    conv1 = tf.layers.conv2d(inputs = X, filters = 48, kernel_size = 3, strides = 1, padding = "same", activation = tf.nn.relu, name = "conv1")
+    conv1 = tf.layers.conv2d(inputs = X, filters = 32, kernel_size = 3, strides = 1, padding = "same", activation = tf.nn.relu, name = "conv1")
     b_norm1 = tf.layers.batch_normalization(inputs = conv1, axis = -1, momentum = 0.99, epsilon = 0.001, center = True, scale = True, name = "batch_norm1")
-    conv2 = tf.layers.conv2d(inputs = b_norm1, filters = 48, kernel_size = 3, strides = 1, padding = "same", activation = tf.nn.relu, name = "conv2")
+    conv2 = tf.layers.conv2d(inputs = b_norm1, filters = 64, kernel_size = 3, strides = 1, padding = "same", activation = tf.nn.relu, name = "conv2")
     pool1 = tf.layers.max_pooling2d(inputs = conv2, pool_size = 2, strides=2, name = "max_pool1")
     drop1 = tf.layers.dropout(inputs = pool1,  rate = drop_1, noise_shape = None, seed = None, name = "dropout1")
 
-    conv3 = tf.layers.conv2d(inputs = drop1, filters = 96, kernel_size = 3, strides = 1, padding = "same", activation = tf.nn.relu, name = "conv3")
+    conv3 = tf.layers.conv2d(inputs = drop1, filters = 128, kernel_size = 3, strides = 1, padding = "same", activation = tf.nn.relu, name = "conv3")
     b_norm2 = tf.layers.batch_normalization(inputs = conv3, axis = -1, momentum = 0.99, epsilon = 0.001, center = True, scale = True, name = "batch_norm2")
-    conv4 = tf.layers.conv2d(inputs = b_norm2, filters = 96, kernel_size = 3, strides = 1, padding = "same", activation = tf.nn.relu, name = "conv4")
+    conv4 = tf.layers.conv2d(inputs = b_norm2, filters = 128, kernel_size = 3, strides = 1, padding = "same", activation = tf.nn.relu, name = "conv4")
     pool2 = tf.layers.max_pooling2d(inputs = conv4, pool_size = 2, strides=2, name = "max_pool2")
     drop2 = tf.layers.dropout(inputs = pool2,  rate = drop_1, noise_shape = None, seed = None, name = "dropout2")
 
-    conv5 = tf.layers.conv2d(inputs = drop2, filters = 192, kernel_size = 3, strides = 1, padding = "same", activation = tf.nn.relu, name = "conv5")
-    b_norm3 = tf.layers.batch_normalization(inputs = conv5, axis = -1, momentum = 0.99, epsilon = 0.001, center = True, scale = True, name = "batch_norm3")
-    conv6 = tf.layers.conv2d(inputs = b_norm3, filters = 192, kernel_size = 3, strides = 1, padding = "same", activation = tf.nn.relu, name = "conv6")
-    pool3 = tf.layers.max_pooling2d(inputs = conv6, pool_size = 2, strides=2, name = "max_pool3")
-    drop3 = tf.layers.dropout(inputs = pool3,  rate = drop_1,    noise_shape = None, seed = None, name = "dropout3")
-
-    pool1_flat = tf.reshape(drop3, [-1, 4 * 4* 192])
-    dense1 = tf.layers.dense(inputs = pool1_flat, units = 512, activation = tf.nn.relu, name = "dense1")
-    drop4 = tf.layers.dropout(inputs = dense1,  rate = drop_2, noise_shape = None, seed = None, name = "dropout4")
-    dense2 = tf.layers.dense(inputs = drop4, units = 256, activation = tf.nn.relu, name = "dense2")
-    drop5 = tf.layers.dropout(inputs = dense2,  rate = drop_2, noise_shape = None, seed = None, name = "dropout5")
-    logits = tf.layers.dense(inputs = drop5, units = 10)
+    pool1_flat = tf.reshape(drop2, [-1, 8 * 8* 128])
+    dense1 = tf.layers.dense(inputs = pool1_flat, units = 1024, activation = tf.nn.relu, name = "dense1")
+    drop3 = tf.layers.dropout(inputs = dense1,  rate = drop_2, noise_shape = None, seed = None, name = "dropout3")
+    dense2 = tf.layers.dense(inputs = drop3, units = 512, activation = tf.nn.relu, name = "dense2")
+    drop4 = tf.layers.dropout(inputs = dense2,  rate = drop_2, noise_shape = None, seed = None, name = "dropout4")
+    logits = tf.layers.dense(inputs = drop4, units = 10)
     out = tf.nn.softmax(logits, name="softmax_output")
 
     return out
@@ -61,58 +83,48 @@ def nn():
 
     summary_op = tf.summary.merge_all()
     init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
     writer = tf.summary.FileWriter(logs_path, graph=tf.get_default_graph())
 
-    fo = open(path, 'rb')
-    dict_ = pickle.load(fo, encoding='latin1')
-    label = dict_['labels']
-    data = dict_['data']
-
-    train_y = []
-    train_x = []
-    for i in range(int(len(data))):
-        temp1 = np.zeros(10)
-        temp1[label[i]] = 1
-        train_y.append(temp1)
-        img = np.zeros([32,32,3])
-        for j in range(len(data[i])):
-            if(j<1024):
-                temp = int(j/32)
-                temp2 = j - (32*temp)
-                img[temp][temp2][0] = data[i][j]
-            elif(j<2048):
-                temp = int((j-1024)/32)
-                temp2 = (j-1024) - (32*temp)
-                img[temp][temp2][1] = data[i][j]
-            else:
-                temp = int((j-2048)/32)
-                temp2 = (j-2048) - (32*temp)
-                img[temp][temp2][2] = data[i][j]
-        if(i<20):
-            cv2.imwrite("img{}.png".format(i),img)
-        train_x.append(img)
-
-    train_size = int(len(train_x)*0.8)
     with tf.Session() as sess:
-        sess.run(init)
+        saver.restore(sess,"./freeze/model.ckpt")
+        #sess.run(init)
+        pt = 0
         for epoch in range(epochs):
             epoch_loss = 0
-            i = 0
-            while i < (train_size-batch_size):
-                start = i
-                end = i+batch_size
-                batch_x = np.array(train_x[start:end])
-                batch_x /= 255
-                batch_y = np.array(train_y[start:end])
-                c = 0
-                _, c, pred, summary, acc = sess.run([optimizer, cost, prediction, summary_op, accuracy], feed_dict={X: batch_x, Y_: batch_y, lr: learning_rate, drop_1: 0.25, drop_2: 0.5})
-                writer.add_summary(summary, epoch * batch_size + i)
-                epoch_loss += c
-                i+=batch_size
-            print('Epoch: ',epoch+1,' completed out of ',epochs,' loss: ',epoch_loss, "Accuracy: ", acc)
-        batch_x = np.array(train_x[train_size:len(train_x)])
-        batch_y = np.array(train_y[train_size:len(train_x)])
-        print('Validation Data Accuracy: ',accuracy.eval({X: batch_x, Y_:batch_y}))
+            for j in range(4):
+                train_x, train_y = next_batch(j+1)
+                train_size = len(train_x)
+                i =0
+                while i < (train_size-batch_size):
+                    start = i
+                    end = i+batch_size
+                    batch_x = np.array(train_x[start:end])
+                    batch_x = batch_x/255
+                    batch_y = np.array(train_y[start:end])
+                    c = 0
+                    _, c, pred, summary, acc = sess.run([optimizer, cost, prediction, summary_op, accuracy], feed_dict={X: batch_x, Y_: batch_y, lr: learning_rate, drop_1: 0.25, drop_2: 0.5})
+                    writer.add_summary(summary, pt)
+                    pt += 1
+                    epoch_loss += c
+                    i+=batch_size
+            save_path = saver.save(sess, "./freeze/model.ckpt")
+            batch_x, batch_y = next_batch(5)
+            batch_x = batch_x/255
+            sum_ = 0
+            for i__ in range(0,10000,100):
+                val_acc = accuracy.eval({X: batch_x[i__:i__+100], Y_:batch_y[i__:i__+100]})
+                sum_ += val_acc
+            sum_ = sum_/100
+            print(' Epoch: ',epoch+1,' completed out of: ',epochs,' train_acc: ', acc, ' val_acc: ', sum_)
+        batch_x, batch_y = test_batch()
+        batch_x = batch_x/255
+        sum_ = 0
+        for i__ in range(0,10000,100):
+            val_acc = accuracy.eval({X: batch_x[i__:i__+100], Y_:batch_y[i__:i__+100]})
+            sum_ += val_acc
+        sum_ = sum_/100
+        print(' Test_acc: ', sum_)
 
 def main():
     nn()
